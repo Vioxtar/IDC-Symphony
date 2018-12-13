31d3c7f7971f14a15160bfd700064e0d9ccc086d
Vioxtar <virustorment@gmail.com>
2018-12-13 15:29:53 +0200

    Deleted .mdb, initial table read

--- src/IDCSymphony.java
+++ src/IDCSymphony.java
@@ -3,20 +3,25 @@ import java.io.IOException;
 
 import com.healthmarketscience.jackcess.Database;
 import com.healthmarketscience.jackcess.DatabaseBuilder;
+import com.healthmarketscience.jackcess.Row;
+import com.healthmarketscience.jackcess.Table;
 
 
 public class IDCSymphony {
 
     public static void main(String [] args) {
-        try {
-            Database db = new DatabaseBuilder(new File("data/IDC Events.mdb"))
-                .setReadOnly(true)
-                .open();
 
-            } catch (IOException e){
+        try (Database db = new DatabaseBuilder(new File("data/IDC Events.accdb"))
+            .setReadOnly(true)
+            .open()) {
+
+            Table table = db.getTable("DiscreteEvents");
+            for(Row row : table) {
+                System.out.println(row);
+            }
+
+        } catch (IOException e){
             e.printStackTrace();
-        } finally {
-
         }
     }
 
