00384d193a5b910ae9a8853f76863485a105813a
Vioxtar <virustorment@gmail.com>
2018-12-14 20:02:31 +0200

    More composer stuff

--- src/Composer.java
+++ src/Composer.java
@@ -1,18 +1,29 @@
-import com.sun.org.glassfish.gmbal.ParameterNames;
 import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
 import org.jfugue.rhythm.Rhythm;
 import org.jfugue.theory.ChordProgression;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Map;
 
+/*
+* A JFugue wrapper for easy composition purposes
+*/
 public class Composer {
 
-    private Pattern symphony;
-    private Map<String, Pattern> usablePatterns;
+    // A map of usable patterns, whose keys are pattern names and values are pattern instances
+    private Map<String, Pattern> loadedPatterns;
 
-    private void loadPatterns() {
-        putPattern("Aye",
+    // The sequence of patterns to construct our composition from
+    private Map<Integer, Sequence> sequences;
+
+    // Keeps track of unique sequence IDs for easy sequence management
+    private int seqID = 0;
+
+    // Load all usable patterns from the pattern arsenal
+    private void loadPatterns() { // TODO: We may not need this, instead just have the external class give the composer its own patterns
+        loadPattern("Aye",
             new ChordProgression("I IV V")
                 .distribute("7%6")
                 .allChordsAs("$0 $0 $0 $0 $1 $1 $0 $0 $2 $1 $0 $0")
@@ -20,38 +31,76 @@ public class Composer {
                 .getPattern()
                 .setInstrument("Acoustic_Bass"));
 
-        putPattern("Bye",
+        loadPattern("Bye",
             new Rhythm()
                 .addLayer("O..oO...O..oOO..")
                 .addLayer("..S...S...S...S.")
                 .addLayer("````````````````")
                 .addLayer("...............+")
                 .getPattern());
-
     }
 
-    private void loadPattern(String name, Pattern pattern) {
-        patterns.put(name, pattern);
+    public void loadPatternsFromDirectory(File dir) {
+        // TODO: Implement :DDD
+    }
+
+    // Loads a pattern to be made usable by the composer
+    public void loadPattern(String name, Pattern pattern) {
+        loadedPatterns.put(name, pattern);
     }
 
     public Composer() {
-        symphony = new Pattern();
-        putPatterns();
+        // Load all usable patterns
+        loadPatterns();
     }
 
-    // Called at the very end
-    public void playSymphony() {
+    // Called at the very end - plays the final composition
+    public void play() {
+        // Compose our composition
+        Pattern composition = new Pattern();
+
+        // Play the composition
         Player player = new Player();
-        player.play(symphony);
+        player.play(composition);
     }
 
-    public void addSeq(String seqName, byte instrument, byte voice, float time, short reps) {
+    // Add a sequence to the composition
+    public int addSequence(String seqName, byte instrument, byte voice, float time, short reps) {
+
+        seqID++;
+
+        // Add the sequence
+        Sequence seq = new Sequence(seqName, instrument, voice, time, reps);
+        sequences.put(seqID, seq);
+
+        // Return the sequence ID
+
+        return seqID;
 
         // TODO: Return an ID representing the pattern placement to be used in remSeq
     }
 
-    // TODO: public void remSeq (some ID) {...}
+    // Remove a sequence from the composition
+    public void remSequence(int id) {
+        sequences.remove(id);
+    }
 
+    // A sequence is simply a packet of information containing the sequence name, the instrument to be used,
+    // the voice to be played on, the time the sequence should play, and the repetitions of the sequence.
+    private class Sequence{
 
+        String seqName;
+        byte instrument;
+        byte voice;
+        float time;
+        short reps;
 
+        public Sequence(String seqName, byte instrument, byte voice, float time, short reps) {
+            this.seqName = seqName;
+            this.instrument = instrument;
+            this.voice = voice;
+            this.time = time;
+            this.reps = reps;
+        }
+    }
 }
--- src/IDCSymphony.java
+++ src/IDCSymphony.java
@@ -1,6 +1,5 @@
 import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
-import org.jfugue.rhythm.Rhythm;
 
 import java.sql.*;
 
@@ -9,8 +8,9 @@ public class IDCSymphony {
 
     public static void main(String [] args) throws ClassNotFoundException {
 
+        System.out.println(p1 == p2);
 
-
+        // Connect to database and perform a test read
         Connection connection = null;
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:data/IDC Events.db");
@@ -20,7 +20,7 @@ public class IDCSymphony {
             ResultSetMetaData blaMD = bla.getMetaData();
             while (bla.next()) {
                 for (int col = 1; col <= blaMD.getColumnCount(); col++) {
-                    System.out.println(bla.getObject(col));
+//                    System.out.println(bla.getObject(col));
                 }
             }
 
