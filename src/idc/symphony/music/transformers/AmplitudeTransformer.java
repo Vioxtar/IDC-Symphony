4e93ec93b65c9ebb7b70e8f446a1b542f3858d05
Tomer Cohen <45826377+Grempington@users.noreply.github.com>
2019-01-08 16:11:46 +0200

    Added pattern amplitude transformer

---
+++ src/idc/symphony/music/transformers/AmplitudeTransformer.java
@@ -0,0 +1,36 @@
+package idc.symphony.music.transformers;
+
+import org.jfugue.parser.ChainingParserListenerAdapter;
+import org.jfugue.parser.ParserListenerAdapter;
+import org.jfugue.pattern.Pattern;
+import org.jfugue.theory.Note;
+import org.staccato.StaccatoParserListener;
+
+public class AmplitudeTransformer extends ChainingParserListenerAdapter {
+    private static AmplitudeTransformer instance = new AmplitudeTransformer();
+
+    private float fraction;
+
+    public static Pattern setRelativeAmp(float fraction, Pattern pattern) {
+        instance.fraction = fraction;
+        StaccatoParserListener builder = new StaccatoParserListener();
+        instance.addParserListener(builder);
+        pattern.transform(instance);
+        instance.removeParserListener(builder);
+
+        return builder.getPattern();
+    }
+
+    private AmplitudeTransformer() {
+        // Un-instantiable
+    }
+
+    @Override
+    public void onNoteParsed(Note note) {
+        if (!note.isRest()) {
+            note.setOnVelocity((byte)Math.round(note.getOnVelocity() * fraction));
+        }
+
+        fireNoteParsed(note);
+    }
+}
