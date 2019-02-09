0c51c3fedb2829861d38bc50070a4de315d65a14
Tomer Cohen <45826377+Grempington@users.noreply.github.com>
2019-02-09 23:11:22 +0200

    The class used for the context extension, should've committed it first.

---
+++ src/idc/symphony/music/conducting/commands/shared/ContextExtension.java
@@ -0,0 +1,23 @@
+package idc.symphony.music.conducting.commands.shared;
+
+import idc.symphony.data.FacultyData;
+import java.util.Map;
+
+public class ContextExtension {
+    public Map<FacultyData, Float> facultyPrevalence;
+    public Map<FacultyData, Boolean> facultyPlayed;
+    public float intensity = 0;
+
+    public ContextExtension() {
+    }
+
+    public float getFactoredPrevalence(FacultyData faculty) {
+        float result = facultyPrevalence.get(faculty);
+
+        if (facultyPlayed.get(faculty)) {
+            //result *= 1000;
+        }
+
+        return result;
+    }
+}
